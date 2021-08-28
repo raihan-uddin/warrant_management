d28c8f297044c20a44b942515bcfe2c9c89a1316
raihan-uddin <blackedition.me@gmail.com>
2021-08-28 08:14:21 +0600

    model design complete

--- geo_spatial/models.py
+++ geo_spatial/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class District(models.Model):
     name = models.CharField(max_length=255)
     name_bn = models.CharField(max_length=255)
-    code = models.CharField(max_length=255)
+    code = models.CharField(max_length=255, null=True)
     status = models.BooleanField(default=True)
 
     def __str__(self):
@@ -14,15 +14,25 @@ class District(models.Model):
 
 class Thana(models.Model):
     name = models.CharField(max_length=255)
-    district = models.ForeignKey(District, on_delete=models.CASCADE, related_name='thanas')
+    district = models.ForeignKey(District, on_delete=models.RESTRICT, related_name='thanas')
     name_bn = models.CharField(max_length=255)
-    code = models.CharField(max_length=255)
+    code = models.CharField(max_length=255, null=True)
     status = models.BooleanField(default=True)
 
 
 class Union(models.Model):
     name = models.CharField(max_length=255)
-    thana = models.ForeignKey(Thana, on_delete=models.CASCADE, related_name='unions')
+    thana = models.ForeignKey(Thana, on_delete=models.RESTRICT, related_name='unions')
     name_bn = models.CharField(max_length=255)
-    code = models.CharField(max_length=255)
+    code = models.CharField(max_length=255, null=True)
+    status = models.BooleanField(default=True)
+
+
+class PoliceUnit(models.Model):
+    name = models.CharField(max_length=255)
+    name_bn = models.CharField(max_length=255)
+    contact_person = models.CharField(max_length=255, null=True)
+    contact_no = models.CharField(max_length=255, null=True)
+    email = models.CharField(max_length=255, null=True)
+    remarks = models.TextField(max_length=255, null=True)
     status = models.BooleanField(default=True)
--- warrant/models.py
+++ warrant/models.py
@@ -1,15 +1,31 @@
-from django.db import models
+import datetime
 
-# Create your models here.
-from geo_spatial.models import District, Thana
+from django.db import models
+from geo_spatial.models import District, Thana, Union, PoliceUnit
 from user.models import CustomUser
 
 
 class Warrant(models.Model):
+    entry_date = models.DateTimeField()
     issue_date = models.DateField()
-    district = models.ForeignKey(District, on_delete=models.CASCADE, related_name='warrants')
-    thana = models.ForeignKey(Thana, on_delete=models.CASCADE, related_name='warrants')
-    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='warrants')
+    warrant_type = models.IntegerField()
+    court_name = models.CharField(max_length=255)
+    warrant_person_name_age = models.CharField(max_length=300)
+    warrant_person_father_name = models.CharField(max_length=300)
+    district = models.ForeignKey(District, on_delete=models.RESTRICT, related_name='warrants')
+    thana = models.ForeignKey(Thana, on_delete=models.RESTRICT, related_name='warrants')
+    union = models.ForeignKey(Union, on_delete=models.RESTRICT, related_name='warrants')
+    address = models.TextField(null=True)
+    gr_cr_no = models.IntegerField()
+    gr_cr_year = models.IntegerField(null=True)
+    case_file_station = models.ForeignKey(Thana, on_delete=models.RESTRICT, related_name='warrants')
+    concerned_police_unit = models.ForeignKey(PoliceUnit, on_delete=models.RESTRICT, related_name='warrants')
+    case_type_section = models.CharField(max_length=255)
+    date_of_presentation_in_court = models.DateField(null=True)
+    court_process_no = models.CharField(max_length=255, null=True)
+    court_given_other_info = models.TextField(null=True)
+    created_by = models.ForeignKey(CustomUser, on_delete=models.RESTRICT, related_name='warrants')
+    created_time = models.DateTimeField(default=datetime.datetime)
 
 
 class WarrantFile(models.Model):
