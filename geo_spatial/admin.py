0e6825fb520ad8c0e245d4982ccb7df61a0a6d95
raihan-uddin <blackedition.me@gmail.com>
2021-09-03 20:30:53 +0600

    warrant picture uploaddjango admin advance search

--- geo_spatial/admin.py
+++ geo_spatial/admin.py
@@ -3,7 +3,27 @@ from django.contrib import admin
 # Register your models here.
 from geo_spatial.models import District, Thana, Union, PoliceUnit
 
-admin.site.register(District)
+
+class DistrictAdvanceFilter(admin.ModelAdmin):
+    # list_filter = [
+    #     "name",
+    #     "name_bn",
+    #     "code",
+    #     "status"
+    # ]
+    search_fields = [
+        "name",
+        "name_bn",
+        "status",
+        "code",
+    ]
+    list_display = ('name', 'name_bn', 'code', 'status')
+    # list_display_links = ('name',)
+    list_filter = ('name', 'name_bn', 'code')
+    list_per_page = 10
+
+
+admin.site.register(District, DistrictAdvanceFilter)
 admin.site.register(Thana)
 admin.site.register(Union)
 admin.site.register(PoliceUnit)
